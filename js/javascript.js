f1952f796c501e203adec21818533108bc8f0226
Agustaina09 <115742202+Agustaina09@users.noreply.github.com>
2022-12-20 22:32:54 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,31 @@
+
+
+function cambiarfondo1() {document.body.style.backgroundColor="white";}
+function cambiarfondo2() {document.body.style.backgroundColor="blue";}
+
+function cartelbienvenida() {alert ("¡Hola!Bienvenid@");}
+function cartelautor() {alert ("Hola! Soy Agustina, la creadora de esta página. Gracias por visitarla!");}
+
+
+function roberto() {
+  document.getElementById("luis").innerHTML = "Potrero Digital";
+}
+
+function AmpliarImg(x){
+  x.style.height = "20%";
+  x.style.width = "20%";
+}
+
+function NormalImg(x){
+  x.style.height = "5%";
+  x.style.width = "5%";
+}
+
+function letraarojo() {
+  document.getElementById("red").style.color = "red";
+}
+
+function modoclaro(){
+  document.body.style.backgroundColor="white";
+  document.body.style.color= "black";
+}
